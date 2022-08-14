a22edfe63d83d9d8eb6f86b4f52ba6773e67c5de
compuFacilito <52604513+gabrielibus@users.noreply.github.com>
2022-08-14 09:57:43 -0500

    Update installCLI.bat

--- compufacilito-CLI/installCLI.bat
+++ compufacilito-CLI/installCLI.bat
@@ -9,21 +9,32 @@ fsutil dirty query %systemdrive%  >nul 2>&1 || (
 ::===========================================================================
 
 :: INSTALL CHOCO - 7zip
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin" && choco install 7zip -y -a
 
-:: CREACIÓN DE CARPETA DE INSTALACIÓN
-set installation_path=%userprofile%\Documents\cf_downloader_tmp
-mkdir %installation_path%
-cd %installation_path%
+if not exist "\programData\chocolatey" @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin" && choco install 7zip -y -a
 
-:: REGISTRO EN VARIABLES DE ENTORNO
-setx /M path "%PATH%;%installation_path%\compufacilito"
-
-:: DESCARGA DEL EJECUTABLE
 set api_url=https://script.google.com/macros/s/AKfycbyNg9CZdSYiXcaNa-FeSK3An8lNTUFCQDHG2BVgn7_0yPJijzwswhqZG0QqM9IjO-8HcQ/exec
 set endpoint="?args=install,compufacilito"
-curl -L "%api_url%%endpoint%" > start.bat
-start.bat
-echo:
-echo Armado y listo!
-pause
+
+:: CREACIÓN DE CARPETA DE INSTALACIÓN
+set installation_path=C:\tmp_cf
+
+if not exist %installation_path% (
+    mkdir %installation_path%
+    cd %installation_path%
+
+    :: REGISTRO EN VARIABLES DE ENTORNO
+    setx /M path "%PATH%;%installation_path%\compufacilito"
+
+    :: DESCARGA DEL EJECUTABLE
+    curl -L "%api_url%%endpoint%" > start.bat
+    start.bat
+    echo:
+    echo Armado y listo!
+    pause
+) ELSE (
+    echo cf ya ha sido instalado
+    pause
+)
+
+
+
